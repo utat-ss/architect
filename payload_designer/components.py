734662d8c45232962eee44d44b0e90c575de8cd7
DM1122 <34867698+DM1122@users.noreply.github.com>
2021-12-27 00:26:49 -0500

    Configure repository

---
+++ payload_designer/components.py
@@ -0,0 +1,16 @@
+"""Component classes."""
+
+
+class SRTransmissionGrating:
+    def __init__(self):
+        pass
+
+
+class VPHGrating:
+    def __init__(self, dcg_thickness):
+        pass
+
+
+class VPHGrism(VPHGrating):
+    def __init__(self):
+        pass
