688a5a67c7347eb8ac237f6770eae575bea99329
gin-guo <72530527+gin-guo@users.noreply.github.com>
2022-01-23 22:26:41 -0500

    debugged SR Grating functions and unit tests

--- tests/test_Surface_Relief_Grating.py
+++ tests/test_Surface_Relief_Grating.py
@@ -13,7 +13,6 @@ from payload_designer import components
 LOG = logging.getLogger(__name__)
 
 
-
 def test_SRGrating_get_angle_out():
     """Test SRGrating.get_angle_out()."""
 
@@ -30,7 +29,8 @@ def test_SRGrating_get_angle_out():
     beta = SR.get_angle_out()
     LOG.info(f"Diffracted angle: {beta}")
 
-    assert beta == pytest.approx(np.arcsin(12/25))
+    assert beta == pytest.approx(0.3113582086)
+
 
 def test_SRGrating_get_angular_dispersion():
     """Test SRGrating.get_angular_dispersion()."""
@@ -38,31 +38,59 @@ def test_SRGrating_get_angular_dispersion():
     # parameters
     alpha = 10
     lmda = 1600
+    G = 300
+    m = 1
 
     # component instantiation
-    SR = components.SRGrating(alpha=alpha, lmda=lmda)
+    SR = components.SRGrating(alpha=alpha, G=G, lmda=lmda, m=m)
+    beta = SR.get_angle_out()
+    SR = components.SRGrating(alpha=alpha, beta=beta, lmda=lmda)
 
     # evaluation
-    ang_disp = SR.get_angle_out()
+    ang_disp = SR.get_angular_dispersion()
     LOG.info(f"Angular dispersion: {ang_disp}")
 
-    assert ang_disp == pytest.approx(4537)
+    assert ang_disp == pytest.approx(315153.0773)
+
 
 def test_SRGrating_get_resolving_power():
     """Test SRGrating.get_resolving_power()."""
 
     # parameters
     alpha = 10
-    beta = np.arcsin(12/25)
+    G = 300
     lmda = 1600
+    m = 1
     W = 300
 
-
     # component instantiation
+    SR = components.SRGrating(alpha=alpha, G=G, lmda=lmda, m=m)
+    beta = SR.get_angle_out()
     SR = components.SRGrating(alpha=alpha, beta=beta, lmda=lmda, W=W)
 
     # evaluation
-    R = SR.get_angle_out()
+    R = SR.get_resolving_power()
     LOG.info(f"Resolving power: {R}")
 
-    assert R == pytest.approx(0.12)
+    assert R == pytest.approx(0.03357794565)
+
+
+def test_SRGrating_get_anamorphic_amplification():
+    """Test SRGrating.get_anamorphic_amplification()."""
+
+    # parameters
+    alpha = 10
+    G = 300
+    lmda = 1600
+    m = 1
+
+    # component instantiation
+    SR = components.SRGrating(alpha=alpha, G=G, lmda=lmda, m=m)
+    beta = SR.get_angle_out()
+    SR = components.SRGrating(alpha=alpha, beta=beta)
+
+    # evaluation
+    ratio = SR.get_anamorphic_amplification()
+    LOG.info(f"Anamorphic dispersion: {ratio}")
+
+    assert ratio == pytest.approx(0.9666032334)
