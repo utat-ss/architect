a22687a9eb3dec4d39b9cc7463da6a0ad329b5c1
Eman Shayeb <eman.shayeb@mail.utoronto.ca>
2022-01-26 20:29:55 -0700

    Final Code

--- payload_designer/components.py
+++ payload_designer/components.py
@@ -1,6 +1,7 @@
 """Component classes."""
 
 # stdlib
+
 import logging
 
 # external
@@ -12,189 +13,191 @@ from payload_designer.libs import physlib
 LOG = logging.getLogger(__name__)
 
 
-class VPHGrism:
-    """Volume-Phase Holographic grating grism component.
+# class VPHGrism:
+#     """Volume-Phase Holographic grating grism component.
 
-    Args:
-        d (float, optional): DCG thickness. Defaults to None.
-        t (float, optional): transmision ratio. Defaults to None.
-        a_in (float, optional): incident ray angle in degrees. Defaults to None.
-        a_out (float, optional): outgoing ray angle in degrees. Defaults to None.
-        R (float, optional): resolvance. Defaults to None.
-        l (array_like[float], optional): wavelength in nm. Defaults to None.
-        l_g (float, optional): undeviated wavelength in nm. Defaults to None.
-        a (float, optional): apex angle. Defaults to None.
-        m (int, optional): diffraction order. Defaults to None.
-        n_1 (float, optional): external index of refraction. Defaults to None.
-        n_2 (float, optional): prism index of refraction. Defaults to None.
-        n_3 (float, optional): grating substrate index of refraction.
-            Defaults to None.
-        v (float, optional): fringe frequency. Defaults to None.
-        dl (float, optional): spectral resolution. Defaults to None.
-        N (float, optional): Number of illumated fringes. Defaults to None.
-    """
+#     Args:
+#         d (float, optional): DCG thickness. Defaults to None.
+#         t (float, optional): transmision ratio. Defaults to None.
+#         a_in (float, optional): incident ray angle in degrees. Defaults to None.
+#         a_out (float, optional): outgoing ray angle in degrees. Defaults to None.
+#         R (float, optional): resolvance. Defaults to None.
+#         l (array_like[float], optional): wavelength in nm. Defaults to None.
+#         l_g (float, optional): undeviated wavelength in nm. Defaults to None.
+#         a (float, optional): apex angle. Defaults to None.
+#         m (int, optional): diffraction order. Defaults to None.
+#         n_1 (float, optional): external index of refraction. Defaults to None.
+#         n_2 (float, optional): prism index of refraction. Defaults to None.
+#         n_3 (float, optional): grating substrate index of refraction.
+#             Defaults to None.
+#         v (float, optional): fringe frequency. Defaults to None.
+#         dl (float, optional): spectral resolution. Defaults to None.
+#         N (float, optional): Number of illumated fringes. Defaults to None.
+#     """
 
-    def __init__(
-        self,
-        d=None,
-        t=None,
-        a_in=None,
-        a_out=None,
-        R=None,
-        l=None,
-        l_g=None,
-        a=None,
-        m=None,
-        n_1=None,
-        n_2=None,
-        n_3=None,
-        v=None,
-        dl=None,
-        N=None,
-    ):
-        self.d = d
-        self.t = t
-        self.a_in = a_in
-        self.a_out = a_out
-        self.R = R
-        self.l = l
-        self.l_g = l_g
-        self.a = a
-        self.m = m
-        self.n_1 = n_1
-        self.n_2 = n_2
-        self.n_3 = n_3
-        self.v = v
-        self.dl = dl
-        self.N = N
+#     def __init__(
+#         self,
+#         d=None,
+#         t=None,
+#         a_in=None,
+#         a_out=None,
+#         R=None,
+#         l=None,
+#         l_g=None,
+#         a=None,
+#         m=None,
+#         n_1=None,
+#         n_2=None,
+#         n_3=None,
+#         v=None,
+#         dl=None,
+#         N=None,
+#     ):
+#         self.d = d
+#         self.t = t
+#         self.a_in = a_in
+#         self.a_out = a_out
+#         self.R = R
+#         self.l = l
+#         self.l_g = l_g
+#         self.a = a
+#         self.m = m
+#         self.n_1 = n_1
+#         self.n_2 = n_2
+#         self.n_3 = n_3
+#         self.v = v
+#         self.dl = dl
+#         self.N = N
 
-    def get_angle_out(self):
-        """Calculates the outgoing angle from the grism.
+#     def get_angle_out(self):
+#         """Calculates the outgoing angle from the grism.
 
-        Returns:
-            float: outgoing angle in degrees.
-        """
-        assert self.a_in is not None, "a_in is not set."
-        assert self.n_1 is not None, "n_1 is not set."
-        assert self.n_2 is not None, "n_2 is not set."
-        assert self.n_3 is not None, "n_3 is not set."
-        assert self.m is not None, "m is not set."
-        assert self.a is not None, "a is not set."
-        assert self.v is not None, "v is not set."
-        assert self.l is not None, "l is not set."
+#         Returns:
+#             float: outgoing angle in degrees.
+#         """
+#         assert self.a_in is not None, "a_in is not set."
+#         assert self.n_1 is not None, "n_1 is not set."
+#         assert self.n_2 is not None, "n_2 is not set."
+#         assert self.n_3 is not None, "n_3 is not set."
+#         assert self.m is not None, "m is not set."
+#         assert self.a is not None, "a is not set."
+#         assert self.v is not None, "v is not set."
+#         assert self.l is not None, "l is not set."
 
-        # region vectorization
-        a_in = np.array(self.a_in).reshape(-1, 1)
-        n_1 = np.array(self.n_1).reshape(-1, 1)
-        n_2 = np.array(self.n_2).reshape(-1, 1)
-        n_3 = np.array(self.n_3).reshape(-1, 1)
-        m = np.array(self.m).reshape(-1, 1)
-        a = np.array(self.a).reshape(-1, 1)
-        v = np.array(self.v).reshape(-1, 1)
-        l = np.array(self.l).reshape(-1, 1)
-        # endregion
+#         # region vectorization
+#         a_in = np.array(self.a_in).reshape(-1, 1)
+#         n_1 = np.array(self.n_1).reshape(-1, 1)
+#         n_2 = np.array(self.n_2).reshape(-1, 1)
+#         n_3 = np.array(self.n_3).reshape(-1, 1)
+#         m = np.array(self.m).reshape(-1, 1)
+#         a = np.array(self.a).reshape(-1, 1)
+#         v = np.array(self.v).reshape(-1, 1)
+#         l = np.array(self.l).reshape(-1, 1)
+#         # endregion
 
-        # region unit conversions
-        l = l * 10 ** -9  # m to nm
-        a_in = np.radians(a_in)  # deg to rad
-        a = np.radians(self.a)  # deg to rad
-        # endregion
+#         # region unit conversions
+#         l = l * 10 ** -9  # m to nm
+#         a_in = np.radians(a_in)  # deg to rad
+#         a = np.radians(self.a)  # deg to rad
+#         # endregion
 
-        angle_1 = a_in + a
-        angle_2 = physlib.snell_angle_2(angle_1=angle_1, n_1=n_1, n_2=n_2)
-        angle_3 = a - angle_2
-        angle_4 = physlib.snell_angle_2(angle_1=angle_3, n_1=n_2, n_2=n_3)
-        angle_5 = angle_4
-        angle_6 = np.arcsin(np.sin(angle_5) - m * np.matmul(v, np.transpose(l)))
-        angle_7 = angle_6
-        angle_8 = physlib.snell_angle_2(angle_1=angle_7, n_1=n_3, n_2=n_2)
-        angle_9 = angle_8 - a
-        angle_10 = physlib.snell_angle_2(angle_1=angle_9, n_1=n_2, n_2=n_1)
-        angle_out = angle_10 + a
+#         angle_1 = a_in + a
+#         angle_2 = physlib.snell_angle_2(angle_1=angle_1, n_1=n_1, n_2=n_2)
+#         angle_3 = a - angle_2
+#         angle_4 = physlib.snell_angle_2(angle_1=angle_3, n_1=n_2, n_2=n_3)
+#         angle_5 = angle_4
+#         angle_6 = np.arcsin(np.sin(angle_5) - m * np.matmul(v, np.transpose(l)))
+#         angle_7 = angle_6
+#         angle_8 = physlib.snell_angle_2(angle_1=angle_7, n_1=n_3, n_2=n_2)
+#         angle_9 = angle_8 - a
+#         angle_10 = physlib.snell_angle_2(angle_1=angle_9, n_1=n_2, n_2=n_1)
+#         angle_out = angle_10 + a
 
-        angle_out = np.degrees(angle_out)
+#         angle_out = np.degrees(angle_out)
 
-        return angle_out
+#         return angle_out
 
-    def get_resolvance(self):
-        """Caculates the grism resolvance.
+#     def get_resolvance(self):
+#         """Caculates the grism resolvance.
 
-        Raises:
-            ValueError: if required parameters are not set.
+#         Raises:
+#             ValueError: if required parameters are not set.
 
-        Returns:
-            float: resolvance.
-        """
-        if self.l is not None and self.dl is not None:
-            R = self.l / self.dl
-        elif self.m is not None and self.N is not None:
-            R = self.m * self.N
-        else:
-            raise ValueError("l and dl or m and N must be set.")
+#         Returns:
+#             float: resolvance.
+#         """
+#         if self.l is not None and self.dl is not None:
+#             R = self.l / self.dl
+#         elif self.m is not None and self.N is not None:
+#             R = self.m * self.N
+#         else:
+#             raise ValueError("l and dl or m and N must be set.")
 
-        return R
+#         return R
 
-    def get_resolution(self):
-        """Caclulates the grism optically-limited spectral resolution.
+#     def get_resolution(self):
+#         """Caclulates the grism optically-limited spectral resolution.
 
-        Raises:
-            ValueError: if required parameters are not set.
+#         Raises:
+#             ValueError: if required parameters are not set.
 
-        Returns:
-            float: resolution in nm.
-        """
-        if self.l is not None and self.R is not None:
-            dl = self.l / self.R
-        elif self.l is not None and self.m is not None and self.N is not None:
-            dl = self.l / (self.m * self.N)
-        else:
-            raise ValueError("l and R or l and m and N must be set.")
+#         Returns:
+#             float: resolution in nm.
+#         """
+#         if self.l is not None and self.R is not None:
+#             dl = self.l / self.R
+#         elif self.l is not None and self.m is not None and self.N is not None:
+#             dl = self.l / (self.m * self.N)
+#         else:
+#             raise ValueError("l and R or l and m and N must be set.")
 
-        return dl
+#         return dl
 
 
-class ThinFocuser:
-    """Thin lens focuser component.
+# class ThinFocuser:
+#     """Thin lens focuser component.
 
-    Args:
-        f (float, optional): focal length. Defaults to None.
-        h (float, optional): image height above optical axis. Defaults to None.
-        a_in (float, optional): incoming ray angle relative to optical axis.
-            Defaults to None.
-    """
+#     Args:
+#         f (float, optional): focal length. Defaults to None.
+#         h (float, optional): image height above optical axis. Defaults to None.
+#         a_in (float, optional): incoming ray angle relative to optical axis.
+#             Defaults to None.
+#     """
 
-    def __init__(self, f=None, h=None, a_in=None):
+#     def __init__(self, f=None, h=None, a_in=None):
 
-        self.f = f
-        self.h = h
-        self.a_in = a_in
+#         self.f = f
+#         self.h = h
+#         self.a_in = a_in
 
-    def get_image_height(self):
-        """Calculate the image height along the focal plane.
+#     def get_image_height(self):
+#         """Calculate the image height along the focal plane.
 
-        Returns:
-            float: image height in m.
-        """
-        assert self.f is not None, "f is not set."
-        assert self.a_in is not None, "a_in is not set."
+#         Returns:
+#             float: image height in m.
+#         """
+#         assert self.f is not None, "f is not set."
+#         assert self.a_in is not None, "a_in is not set."
 
-        # region vectorization
-        f = np.array(self.f).reshape(-1, 1)
-        a_in = np.array(self.a_in).reshape(-1, 1)
-        # endregion
+#         # region vectorization
+#         f = np.array(self.f).reshape(-1, 1)
+#         a_in = np.array(self.a_in).reshape(-1, 1)
+#         # endregion
 
-        # region unit conversions
-        a_in = np.radians(a_in)  # deg to rad
-        # endregion
+#         # region unit conversions
+#         a_in = np.radians(a_in)  # deg to rad
+#         # endregion
 
-        h = np.matmul(f, np.transpose(np.tan(a_in)))
+#         h = np.matmul(f, np.transpose(np.tan(a_in)))
 
-        return h
+#         return h
 
 class BandpassFilter:
     def __init__(
         self,
         A = None, 
+        d = None,
+        delta = None,  
         lambda_theta = None,
         lambda_0 = None, 
         theta = None, 
@@ -205,15 +208,25 @@ class BandpassFilter:
         N = None, 
         n = None, 
         k = None, 
-        phi_e = None, 
+        phi_1 = None,
+        phi_2 = None,
         R = None, 
-        T = None, 
+        R_1 = None, 
+        R_2 = None,
+        T_f = None, 
         T_1 = None, 
         T_2 = None, 
-        omega = None, 
         J = None, 
+        T_w = None,
+        T_1w = None,
+        T_2w = None,
+        F_w = None,
+        F = None,
+        T_0 = None,
         ):
         self.A = A
+        self.d = d
+        self.delta = delta
         self.lambda_theta = lambda_theta
         self.lambda_0 = lambda_0
         self.theta = theta
@@ -224,13 +237,21 @@ class BandpassFilter:
         self.N = N
         self.n = n 
         self.k = k 
-        self.phi_e = phi_e 
+        self.phi_1 = phi_1
+        self.phi_2 = phi_2
         self.R = R
-        self.T = T
+        self.R_1 = R_1
+        self.R_2 = R_2
+        self.T_f = T_f 
         self.T_1 = T_1 
         self.T_2 = T_2 
-        self.omega = omega
         self.J = J
+        self.T_w = T_w
+        self.T_1w = T_1w
+        self.T_2w = T_2w
+        self.T_0 = T_0
+        self.F_w = F_w 
+        self.F = F
         
     def effective_refractive_index(self):
         assert self.epsilon_1 is not None, "epsilon_1 is not set."
@@ -245,12 +266,12 @@ class BandpassFilter:
         return n_star
         
     def phase_shift(self):
-        assert self.lambda_0 is not None, "lambda_not is not set."
+        assert self.lambda_0 is not None, "lambda_0 is not set."
         assert self.n_0 is not None, "n_0 is not set."
         assert self.n_star is not None, "n_star is not set."
         assert self.theta is not None, "theta is not set."
         
-        lambda_theta = self.lambda_0*(1 - ((self.n_0/self.n_star)*np.sin(self.theta)))**2)
+        lambda_theta = self.lambda_0*(1 - ((self.n_0/self.n_star)*np.sin(self.theta))**2)**0.5
         
         return lambda_theta
     
@@ -261,6 +282,15 @@ class BandpassFilter:
         R = ((self.n_star - self.n_0)/(self.n_star + self.n_0))**2
         return R
         
-    def transmitted_beam(self):
+    def transmitted_beam_system(self):
+        assert self.R_1 is not None, "R_1 is not set."
+        assert self.R_2 is not None, "R_2 is not set."
+        assert self.theta is not None, "theta is not set."
+        assert self.T_1 is not None, "T_1 is not set."
+        assert self.T_2 is not None, "T_2 is not set."
+        assert self.phi_1 is not None, "phi_1 is not set."
+        assert self.phi_2 is not None, "phi_2 is not set."
         
-    
+        
+        T_f = ((self.T_1*self.T_2)/(1-(self.R_1*self.R_2)**0.5)**2)*(1/(1+((4*(self.R_1*self.R_2)**0.5)/(1-(self.R_1*self.R_2)**0.5)**2)*(np.sin(0.5*self.phi_1*self.phi_2-self.theta))**2))
+        return T_f
