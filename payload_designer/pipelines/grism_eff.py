74959fd6ebe168e1fa1c2976cc901843df199b55
gjerazik <gjerazikejsi@gmail.com>
2022-02-20 12:37:32 -0500

    add new plotting format

--- payload_designer/pipelines/grism_eff.py
+++ payload_designer/pipelines/grism_eff.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 # external
 import numpy as np
+import pandas as pd
 
 # project
 from payload_designer.components import diffractors
@@ -28,8 +29,8 @@ LOG = logging.getLogger(__name__)
 # endregion
 
 # region parameter config
-lmbda = np.linspace(start=1500, stop=1700, num=10)  # [nm]
-v = np.linspace(start=300, stop=1200, num=200)  # [L/mm]
+l = np.linspace(start=1500, stop=1700, num=10)  # [nm]
+v = np.linspace(start=900, stop=1200, num=200)  # [L/mm]
 a_in = np.linspace(start=0, stop=20, num=20)
 d = np.linspace(start=0.5, stop=4, num=20)
 n_g = 0.1
@@ -44,7 +45,7 @@ if __name__ == "__main__":
     # region component instantiation
     diffractor = diffractors.VPHGrism(
         m=1,
-        l=lmbda,
+        l=l,
         v=v,
         a_in=a_in,
         eff_mat=eff_mat,
@@ -62,8 +63,34 @@ if __name__ == "__main__":
     # endregion
 
     # region plots
-    plotlib.line(x=lmbda, y=eff)
-    plotlib.line(x=v, y=eff)
-    plotlib.line(x=a_in, y=eff)
-    plotlib.line(x=d, y=eff)
+    dfd = {"a_in [°]": a_in.flatten(), "d [μm]": d.flatten(), "l [nm]": l.flatten(), "v [lines/mm]": v.flatten(), "a [°]": a.flatten(), 
+    "eff_mat []": eff_mat.flatten(), "n_g []": n_g.flatten(),"n_1 []": n_1.flatten(), "n_2 []": n_2.flatten(), "n_3 []": n_3.flatten(), "eff []": eff.flatten()}
+    df = pd.DataFrame(data=dfd)
+    LOG.debug(df)
+
+    plotlib.line(
+        df=df,
+        x="a_in [°]",
+        y="eff []",
+        title="Grism Efficiency vs Incident Ray Angle",
+    )
+    plotlib.line(
+        df=df,
+        x="v [lines/mm]",
+        y="eff []",
+        title="Grism Efficiency vs Fringe Frequency",
+    )
+    plotlib.line(
+        df=df,
+        x="f [mm]",
+        y="angle [°]",
+        title="Grism Efficiency vs Wavelength",
+    )
+    plotlib.line(
+        df=df,
+        x="d [μm]",
+        y="eff []",
+        title="Grism Efficiency vs DCG Thickness",
+    )
+
     # endregion
