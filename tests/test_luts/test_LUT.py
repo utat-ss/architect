06f32fedd228f0ff9f8271064a54035a38e0b4f2
David Maranto <d.maranto1122@gmail.com>
2022-11-01 13:58:00 -0400

    Rename LUT test file

--- tests/test_luts/test_LUT.py
+++ tests/test_luts/test_LUT.py
@@ -13,8 +13,8 @@ from architect import luts
 LOG = logging.getLogger(__name__)
 
 
-def test_LUT_call():
-    """Test the LUT class call method."""
+def test_call():
+    """Test call method."""
 
     lut = luts.load("test_lut")
     LOG.debug(lut)
@@ -24,3 +24,20 @@ def test_LUT_call():
 
     y = lut(x)
     LOG.debug(f"Output y:\n{y}")
+
+
+def test_mul():
+    """Test multiplication method."""
+
+    lut = luts.load("test_lut")
+    lut_2 = luts.load("test_lut")
+
+    x = np.linspace(start=1500, stop=1700, num=16) * unit.nm
+    x_2 = np.linspace(start=1500, stop=1700, num=16) * unit.nm
+
+    y = lut(x)
+    y_2 = lut_2(x_2)
+
+    result = y * y_2
+
+    LOG.info(result)
