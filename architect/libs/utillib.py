d271a427d2599185f3b785c80611e15372b3db9e
David Maranto <d.maranto1122@gmail.com>
2022-11-08 16:08:17 -0500

    Refactor utilib.hypercast

--- architect/libs/utillib.py
+++ architect/libs/utillib.py
@@ -4,41 +4,30 @@ import numpy as np
 from astropy.units.quantity import Quantity
 
 
-def orient_tensor(a, dim: int, dims: int) -> np.ndarray:
-    """Orient a tensor in a given dimensionality.
+def hypercast(*arrays) -> list[np.ndarray]:
+    """Broadcast a given set of 1D arrays into hypercubes of the same shape.
 
-    Args:
-        a: array tensor.
-        dim: index of the dimension axis on which to orient the tensor.
-        dims: dimensionality of the tensor space.
-
-    Returns:
-        The oriented tensor.
-
-    """
-    shape = [1] * dims
-    shape[dim] = -1
-
-    return np.reshape(a=a, newshape=shape)
-
-
-def orient_and_broadcast(a, dim: int, shape) -> np.ndarray:
-    """Orient and broadcast a tensor into a given dimensionality.
-
-    Args:
-        a: array tensor.
-        dim: index of the dimension axis on which to orient the tensor.
-        shape: dimensionality and size of broadcasted tensor.
-
-    Returns:
-        The broadcasted tensor.
+    Will orient the arrays in hyperspace according to the order in which they are
+    passed.
 
     """
 
-    a_oriented = orient_tensor(a=a, dim=dim, dims=len(shape))
-    a_broadcasted = np.broadcast_to(array=a_oriented, shape=shape)
+    hypershape = [a.size for a in arrays]
 
-    if isinstance(a, Quantity):
-        a_broadcasted *= a.unit
+    hyperarrays = []
+    for i, array in enumerate(arrays):
+        # orient the array in hyperspace
+        orient_shape = [1] * len(hypershape)
+        orient_shape[i] = -1
 
-    return a_broadcasted
+        array_oriented = np.reshape(a=array, newshape=orient_shape)
+
+        # broadcast the array into a hypercube
+        array_broadcasted = np.broadcast_to(array=array_oriented, shape=hypershape)
+
+        if isinstance(array, Quantity):
+            array_broadcasted *= array.unit
+
+        hyperarrays.append(array_broadcasted)
+
+    return hyperarrays
--- tests/test_libs/test_utillib.py
+++ tests/test_libs/test_utillib.py
@@ -3,18 +3,41 @@
 # stdlib
 import logging
 
+# external
+import numpy as np
+
 # project
 from architect.libs import utillib
 
 LOG = logging.getLogger(__name__)
 
 
-def test_orient_tensor():
-    """Test the orient_tensor function."""
-    ans = (1, 4, 1)
-    a = [1, 2, 3, 4]
+def test_hypercast_2D():
+    """Test the hypercast function with two arrays."""
 
-    a_orient = utillib.orient_tensor(a=a, dim=1, dims=3)
+    a = np.array([1, 2, 3])
+    b = np.array([4, 5, 6, 7])
 
-    LOG.debug(f"a_orient shape: {a_orient.shape}, {ans} (ans)")
-    assert a_orient.shape == ans
+    a_hyper, b_hyper = utillib.hypercast(a, b)
+    LOG.info(a_hyper)
+    LOG.info(b_hyper)
+
+    assert a_hyper.shape == (3, 4)
+    assert b_hyper.shape == (3, 4)
+
+
+def test_hypercast_3D():
+    """Test the hypercast function with three arrays."""
+
+    a = np.array([1, 2, 3])
+    b = np.array([4, 5, 6, 7])
+    c = np.array([8, 9, 10, 11, 12])
+
+    a_hyper, b_hyper, c_hyper = utillib.hypercast(a, b, c)
+    LOG.info(a_hyper)
+    LOG.info(b_hyper)
+    LOG.info(c_hyper)
+
+    assert a_hyper.shape == (3, 4, 5)
+    assert b_hyper.shape == (3, 4, 5)
+    assert c_hyper.shape == (3, 4, 5)
